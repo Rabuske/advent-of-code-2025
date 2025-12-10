c9767b19e2da46c453cea935b6baa886c9fb61fc
Felipe Costa <felipe.costa@sap.com>
2025-12-10 13:08:55 -0300

    Day 03 ❄❄❄

--- solution/Day03.cs
+++ solution/Day03.cs
@@ -11,22 +11,8 @@ class Day03 : IDayCommand
 
     foreach (var battery in batteries)
     {
-      // The 10s are more important
-      var max10Value = -1;
-      var max10Index = 0;
-      for (int i = 0; i < battery.Count - 1; i++)
-      {
-        if(battery[i] > max10Value)
-        {
-          max10Value = battery[i];
-          max10Index = i;
-        }
-      }
-
-      // Now find units
-      var maxUnit = battery[(max10Index + 1)..].Max();
-      sumPart01 += max10Value * 10 + maxUnit;
-
+      sumPart01 += GetMaxValueForGivenSize(battery, 2);
+      sumPart02 += GetMaxValueForGivenSize(battery, 12);
     }
 
     return $"""
@@ -34,6 +20,39 @@ class Day03 : IDayCommand
                The new password is: {sumPart02}
             """;
   }
+
+
+  (int value, int index) GetNextMaxValue(List<int> battery, int startIndex, int remainingDigits)
+  {
+      var maxValue = -1;
+      var indexOfMaxValue = 0;
+      for (int i = startIndex; i < battery.Count - remainingDigits; i++)
+      {
+        if(battery[i] > maxValue)
+        {
+          maxValue = battery[i];
+          indexOfMaxValue = i;
+        }
+      }   
+      return (maxValue, indexOfMaxValue);
+  }
+
+  long GetMaxValueForGivenSize(List<int> battery, int size)
+  {
+      var startIndex = 0;
+      var number = new List<int>();
+      for (int i = 0; i < size; i++)
+      {
+        var remainingDigits = size - i - 1;
+        var nextNumberAndIndex = GetNextMaxValue(battery, startIndex, remainingDigits);
+        startIndex = nextNumberAndIndex.index + 1;
+        number.Add(nextNumberAndIndex.value);
+      }
+
+      return long.Parse(String.Join("", number));    
+  }
+
+
 }
 
  
